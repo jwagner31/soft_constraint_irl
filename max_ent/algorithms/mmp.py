f2a4746d63bfbd3bd09511385c108085740b7814
jwagner31 <jwagner3@tulane.edu>
2023-10-12 15:29:17 -0500

    changes

--- max_ent/algorithms/mmp.py
+++ max_ent/algorithms/mmp.py
@@ -70,30 +70,20 @@ def forward(p_transition, p_initial, policy, terminal, eps=1e-5):
 
 
 
-### Input: Feature Matrix (phi), D (demonstration set)
+### Input: Feature Matrix (phi), D (demonstration set), discount (gamma)
 ### Output: Expected Feature Frequencies 
-def ef_from_trajectories(features, trajectories):
+def ef_from_trajectories(features, trajectories, discount):
     n_features = features.shape[-1]
 
     fe = np.zeros(n_features)
 
     for t in trajectories:
+        time_step = 0
         for s, a, s_ in t.transitions():
-            fe += features[s, a, s_, :]
+            fe += features[s, a, s_, :] * discount ** time_step
+            time_step += 1
 
     return fe / len(trajectories)
-
-def feature_expectation_from_policy(features, policy):
-    n_features = features.shape[-1]
-    n_states, n_actions, _, _ = features.shape
-
-    fe = np.zeros(n_features)
-
-    for s in range(n_states):
-        for a in range(n_actions):
-            fe += policy[s, a] * features[s, a, :, :]
-
-    return fe
 
 
 def sigmoid(arry):
@@ -123,6 +113,10 @@ def sample_trajectories_from_policy(n_states, policy, start, terminal, n_traject
                                        world, policy_exec, initial, terminal))
     return Demonstration(tjs, None)
 
+def _softmax(x1, x2):
+    x_max = np.maximum(x1, x2)
+    x_min = np.minimum(x1, x2)
+    return x_max + np.log(1.0 + np.exp(x_min - x_max))
 
 #Features shape: (81, 8, 81, 92)  Reward Shape: (81, 8, 81)  Policy shape: (81, 8)
 # P_intial shape: (81, 8)  p_transition shape: (81, 81, 8)
@@ -131,7 +125,6 @@ def sample_trajectories_from_policy(n_states, policy, start, terminal, n_traject
 # https://github.com/rhklite/apprenticeship_inverse_RL/blob/master/Apprenticeship_Inverse_Reinforcement_Learning.ipynb
 
 
-###
 def mmp(nominal_rewards, p_transition, features, terminal, trajectories, optim, init, discount,
          eps=1e-4, eps_error=1e-2, burnout=30, max_iter=10000, max_penalty=200, log=None, initial_omega=None):
 
@@ -141,7 +134,7 @@ def mmp(nominal_rewards, p_transition, features, terminal, trajectories, optim, 
     features[terminal] = 0
 
     # Compute expert feature expectation from expert demonstrations
-    expert_features = ef_from_trajectories(features, trajectories)
+    expert_features = ef_from_trajectories(features, trajectories, discount)
     # Compute probability of a state being the initial start state
     p_initial = initial_probabilities(n_states, n_actions, trajectories)
     # Nominal reward vector is already known
@@ -152,77 +145,69 @@ def mmp(nominal_rewards, p_transition, features, terminal, trajectories, optim, 
     omega = init(n_features) # current weight vector
     if initial_omega is not None:
         omega = initial_omega.copy()
-    delta = mean_error = np.inf 
-    optim.reset(omega)
-
-    margin_list = [] # list of margin for each iteration
-    margin = 1 # current margin; dummy margin set to start
     policy_list = [] # list of policy after each iteration
 
-    feature_expectations = []
-    feature_expectations_temp = []
+    nonexpert_feature_expectations = np.zeros(shape=(0, n_features))
+    nonexpert_feature_expectations_blended = np.zeros(shape=(0, n_features))
 
     reward = nominal_rewards - features @ omega
 
+    # First, compute random policy. To do this, we need a reward function to use for value iteration.
+    q_function, v_function = RL.value_iteration(p_transition, reward, discount)
+    policy = RL.stochastic_policy_from_q_value(q_function) #get policy from running RL with initial reward function
+    policy_exec = T.stochastic_policy_adapter(policy)
+    policy_list.append(policy)
+
     for i in range(burnout):
-        print("Iteration: ", i)
-        if i==0: #initialize variables
-            # First, compute random policy. To do this, we need a reward function to use for value iteration.
-            q_function, v_function = RL.value_iteration(p_transition, reward, discount)
-            policy = RL.stochastic_policy_from_q_value(q_function) #get policy from running RL with initial reward function
-            policy_exec = T.stochastic_policy_adapter(policy)
-            policy_list.append(policy)
-            #compute ef from this policy - renamed to D to match with paper
-            d = forward(p_transition, p_initial, policy, terminal)
-            df = (d[:, :, :, None] * features).sum((0, 1, 2))
-            feature_expectations.append(df)
-            omega_list.append(omega)
-            margin_list.append(margin)
-        else:
-            if i==1:
-                feature_expectations_temp.append(feature_expectations[i-1])
-                omega = expert_features - feature_expectations_temp[i-1]
-                omega_list.append(omega)
-                margin = norm((expert_features-feature_expectations_temp[i-1]),2)
-                margin_list.append(margin)
 
-                print("margin: ", margin_list[i])
-            else:
-                A = feature_expectations_temp[i-2]
-                B=feature_expectations[i-1]-A
-                C=expert_features-feature_expectations_temp[i-2]
-                feature_expectations_temp.append(A+(np.dot(B,C)/np.dot(B,B))*(B))
+        #get latest policy
+        policy = policy_list[-1]
 
-                omega = expert_features - feature_expectations_temp[i-1]
-                omega_list.append(omega)
-                margin = norm((expert_features-feature_expectations_temp[i-1]),2)
-                margin_list.append(margin)
+        #Sample new trajectories
+        initial = np.zeros(n_states)
+        initial[0] = 1.0
+        sample_tjs = T.generate_trajectories(1000, n_states, policy_exec, initial, terminal)
 
-                print("margin: ",margin_list[i]) 
-            
-            #if(margin <= eps):
-             #   break
-            
-            # Compute opimal policy using RL, then use that policy to get feature expectation
-            reward = nominal_rewards - features @ omega
-            q_function, v_function = RL.value_iteration(p_transition, reward, discount)
-            policy = RL.stochastic_policy_from_q_value(q_function) #confused on this step
-            policy_list.append(policy)
-            d = forward(p_transition, p_initial, policy, terminal)
-            df = (d[:, :, :, None] * features).sum((0, 1, 2))
-            feature_expectations.append(df)
+        #Compute feature expectations of new policy and add to list
+        nonexpert_feature_expectations = np.vstack(
+                (
+                    nonexpert_feature_expectations,
+                    nonexpert_feature_expectations(features, sample_tjs, discount)))
+        
 
-            mean_error = np.abs(df - expert_features).mean()
-            print(mean_error)
-            delta = np.max(np.abs(omega_list[i-1] - omega))
-            #if(delta <= eps):
-              #  break
+        mu_e = expert_features 
+        mu_prev = nonexpert_feature_expectations[-1]
+
+        mu_prev_prev = nonexpert_feature_expectations[-2]
+        mu_bar_prev_prev = nonexpert_blended_feature_expectations[-2, :]
+
+        # The below finds the orthogonal projection of the expert's
+        # feature expectations onto the line through mu_prev and
+        # mu_prev_prev
+        mu_bar_prev = mu_bar_prev_prev \
+            + (mu_prev - mu_bar_prev_prev).T \
+                @ (mu_e - mu_bar_prev_prev) \
+            / (mu_prev - mu_bar_prev_prev).T \
+                @ (mu_prev - mu_bar_prev_prev) \
+            * (mu_prev - mu_bar_prev_prev)
+
+        nonexpert_blended_feature_expectations = np.vstack(
+            (
+                nonexpert_blended_feature_expectations,
+                mu_bar_prev
+            )
+        )
+
+        omega = mu_e - mu_bar_prev
+        omega_list.append(omega)
+
+        reward = nominal_rewards - features @ omega
+        error = np.linalg.norm(w)
+        print(error)
+
+        q_new, v_new = RL.value_iteration(p_transition, reward, discount)
+        new_policy = RL.stochastic_policy_from_q_value(q_new)
+        new_policy_exec = T.stochastic_policy_adapter(new_policy)
+        policy_list.append(new_policy)
+
     return omega_list[-1]
-
-
-
-
-def _softmax(x1, x2):
-    x_max = np.maximum(x1, x2)
-    x_min = np.minimum(x1, x2)
-    return x_max + np.log(1.0 + np.exp(x_min - x_max))
