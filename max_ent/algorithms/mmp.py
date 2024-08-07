caea2d57dccd1efd45053ed5a786759cef430902
joey <jwagner3@tulane.edu>
2024-08-06 23:39:08 -0500

    code sorted into functions and helper functions

--- max_ent/algorithms/mmp.py
+++ max_ent/algorithms/mmp.py
@@ -7,126 +7,116 @@ import math
 import max_ent.gridworld.trajectory as T
 import max_ent.optim as O
 import scipy.special
-
-### Input: Feature Matrix (phi), D (demonstration set), discount (gamma)
-### Output: Expected Feature Frequencies 
-def ef_from_trajectories(features, trajectories, discount):
-    n_features = features.shape[-1]
-
-    fe = np.zeros(n_features)
-
-    for t in trajectories:
-        time_step = 1
-        for s, a, s_ in t.transitions():
-            fe += features[s, a, s_, :] * discount ** time_step
-            time_step += 1
-
-    return fe / len(trajectories)
+import max_ent.algorithms.mmp_helper as H
+import max_ent.algorithms.robsvm as R
+from sklearn.svm import SVC
 
 
-### Input: # of states, # of actions, D (Demonstration set)
-### Output: P_start ()
-def initial_probabilities(n_states, n_actions, trajectories):
-    initial = np.zeros((n_states, n_actions))
-    for t in trajectories:
-        s, a, _ = list(t.transitions())[0]
-        initial[s, a] += 1
-    return initial / len(trajectories)
+def initialization():
 
-def sample_trajectories_from_policy(n_states, policy, start, terminal, n_trajectories=200):
-    # set up initial probabilities for trajectory generation
-    initial = np.zeros(n_states)
-    initial[start] = 1.0
 
-    # generate trajectories
-    policy_exec = T.stochastic_policy_adapter(policy)
-    tjs = list(T.generate_trajectories(n_trajectories,
-                                       world, policy_exec, initial, terminal))
-    return Demonstration(tjs, None)
-
+    return True
 
 #Features shape: (81, 8, 81, 92)  Reward Shape: (81, 8, 81)  Policy shape: (81, 8)
 # P_intial shape: (81, 8)  p_transition shape: (81, 81, 8)
 
 
-def mmp(nominal_rewards, p_transition, features, terminal, trajectories, optim, init, discount,
-         eps=1e-4, eps_error=1e-2, burnout=10, max_iter=10000, max_penalty=200, log=None, initial_omega=None):
+def mmp(world, nominal_rewards, p_transition, features, terminal, trajectories, optim, init, discount,
+         eps=1e-4, eps_error=1e-2, burnout=50, max_iter=10000, max_penalty=200, log=None, initial_omega=None, method="projection"):
 
+    ### INITIALIZE VARIABLES
     n_states, n_actions, _, n_features = features.shape
+    features[terminal] = 0  # Don't count transitions that start with a terminal state
+    p_initial = H.initial_probabilities(n_states, n_actions, trajectories) # Compute probability of a state being the initial start state
+    nominal_rewards = np.array(nominal_rewards) # Nominal reward vector is already known
+    initial = np.zeros(world.n_states)
+    initial[0] = 1.0
 
-    # Don't count transitions that start with a terminal state
-    features[terminal] = 0
-
-    # Compute expert feature expectation from expert demonstrations
-    expert_features = ef_from_trajectories(features, trajectories, discount)
-    # Compute probability of a state being the initial start state
-    p_initial = initial_probabilities(n_states, n_actions, trajectories)
-    # Nominal reward vector is already known
-    nominal_rewards = np.array(nominal_rewards)
-
-    ## Variables we will keep track of during iteration
-    omega_list = [] # list of weight vectors for each iteration
-    omega = np.zeros(n_features) # current weight vector
-    if initial_omega is not None:
-        omega = initial_omega.copy()
+    ### TRACKING VARIABLES
+    policy_list  = []
     margin = []
-    policy_list = [] # list of policy after each iteration
+    fe_current = []
+    fe_bar = []
+    omega_c = []
 
-    nonexpert_feature_expectations = []
-    nonexpert_feature_expectations_blended = []
+    fe_expert = H.fe_from_trajectories(features, trajectories, discount)
+    #print(fe_expert)
 
-    reward = nominal_rewards - features @ omega
+    ### Step 3 of MESC-IRL
+    omega_r = np.zeros(n_features)
+    reward_c = nominal_rewards - features @ omega_r
+    reward_c[:,:,:] = 0
 
     # First, compute random policy. To do this, we need a reward function to use for value iteration.
-    q_function, v_function = RL.value_iteration(p_transition, reward, discount)
-    policy = RL.stochastic_policy_from_q_value(q_function) #get policy from running RL with initial reward function
-    policy_exec = T.stochastic_policy_adapter(policy)
+    q_function, v_function = H.value_iteration(p_transition, reward_c, discount)
+    policy = H.stochastic_policy_from_q_value(q_function) #get policy from running RL with initial reward function
+    policy_exec = H.stochastic_policy_adapter(policy)
     policy_list.append(policy)
+    sample_tjs = None
 
     for i in range(burnout):
+        print("Iteration: ", i)
+        if method == "projection":
+            #get latest policy
+            policy = policy_list[-1]
 
-        #get latest policy
-        policy = policy_list[-1]
-
-        #Sample new trajectories
-        initial = np.zeros(n_states)
-        initial[0] = 1.0
-
-        #Compute feature expectations of new policy and add to list
-        if i==0:
-            sample_tjs = T.generate_trajectories_noworld(1000, n_states, p_transition, policy_exec, initial, terminal)
-            nonexpert_feature_expectations.append(ef_from_trajectories(features, sample_tjs, discount))
-            print("expert feature expectation: ", expert_features[0:4])
-            margin.append(1)
-            omega_list.append(omega)
-        else:
-            if i==1:
-                nonexpert_feature_expectations_blended.append(nonexpert_feature_expectations[i-1])
-                omega = expert_features - nonexpert_feature_expectations[i-1]
-                omega_list.append(omega)
-                margin.append(norm((expert_features-nonexpert_feature_expectations_blended[i-1]), 2))
+            #Compute feature expectations of new policy and add to list
+            if i==0:
+                sample_tjs = T.generate_trajectories(1000, world, policy_exec, initial, terminal) #get sample trajectories from initial policy
+                fe_current.append(H.fe_from_trajectories(features, sample_tjs, discount)) # get feature expectation of initial policy using sampled trajectories
+                margin.append(1) # dummy margin to start
+                omega_c.append(np.zeros(n_features)) # dummy omega_c to start
             else:
-                A = nonexpert_feature_expectations_blended[i-2]
-                B = nonexpert_feature_expectations[i-1]-A
-                C = expert_features - nonexpert_feature_expectations_blended[i-2]
-                nonexpert_feature_expectations_blended.append(A+(np.dot(B,C)/np.dot(B,B))*(B))
-                omega = expert_features-nonexpert_feature_expectations_blended[i-1]
-                omega_list.append(omega)
-                margin.append(norm((expert_features-nonexpert_feature_expectations_blended[i-1]), 2))
+                if i==1:
+                    fe_bar.append(fe_current[i-1])
+                    omega_c.append(fe_expert - fe_current[i-1])
+                    margin.append(norm((fe_expert-fe_bar[i-1]), 2))
+                else:
+                    A = fe_bar[i-2]
+                    B = fe_current[i-1]-A
+                    C = fe_expert - fe_bar[i-2]
+                    fe_bar.append(A+(np.dot(B,C)/np.dot(B,B))*(B))
+                    omega_c.append(fe_expert - fe_bar[i-1])
+                    margin.append(norm((fe_expert-fe_bar[i-1]), 2))
+                    print("margin: ", margin[i])
 
-            print("margin: ", margin[-1])
-            print("first few weights: ", omega[0:4])
-            reward = nominal_rewards - features @ omega
-            q_new, v_new = RL.value_iteration(p_transition, reward, discount)
-            new_policy = RL.stochastic_policy_from_q_value(q_new)
-            new_policy_exec = T.stochastic_policy_adapter(new_policy)
-            policy_exec = new_policy_exec
+                reward_c = features @ omega_c[i]
+                #print("reward_c: ", reward_c[1,1,:])
+                q_new, v_new = H.value_iteration(p_transition, reward_c, discount)
+                new_policy = H.stochastic_policy_from_q_value(q_new)  # (s, a) -> p(a | s)
+                new_policy_exec = H.stochastic_policy_adapter(new_policy)
+                policy_exec = new_policy_exec
+                policy_list.append(new_policy)
+                sample_tjs = T.generate_trajectories(1000, world, policy_exec, initial, terminal)
+                fe_current.append(H.fe_from_trajectories(features, sample_tjs, discount))
+        else: 
+            nonexpert_feature_expectations = np.zeros(shape = (0, n_features))
+
+            #get latest policy
+            policy = policy_list[-1]
+            policy_exec = H.stochastic_policy_adapter(policy)
+            sample_tjs = T.generate_trajectories(1000, world, policy_exec, initial, terminal)
+            nonexpert_feature_expectations = np.vstack((nonexpert_feature_expectations, H.fe_from_trajectories(features, sample_tjs, discount)))
+
+            X = np.vstack((fe_expert, nonexpert_feature_expectations))
+            y = np.array([1] + [-1] * len(nonexpert_feature_expectations))
+            # Train a standard SVM
+            clf = SVC(kernel='linear')
+            clf.fit(X, y)
+
+            # Get the weight vector w and intercept b from the trained SVM
+            w = clf.coef_[0]
+            b = clf.intercept_[0]
+
+            print(np.linalg.norm(w))
+
+            reward_c = features @ w
+            q_new, v_new = H.value_iteration(p_transition, reward_c, discount)
+            new_policy = H.stochastic_policy_from_q_value(q_new)
             policy_list.append(new_policy)
-            sample_tjs = T.generate_trajectories_noworld(1000, n_states, p_transition, policy_exec, initial, terminal)
-            nonexpert_feature_expectations.append(ef_from_trajectories(features, sample_tjs, discount))
 
     #print(nonexpert_feature_expectations[-1])
-    return omega_list[-1]
+    return reward_c, sample_tjs
 
 
 
